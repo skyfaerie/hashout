11a8caad89bf9ea123ba291b4d377c390cac9cf9
Aisha Green <skyfaerie22@gmail.com>
2014-05-04 10:41:41 -0400

    added express rollback

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,19 @@
 /*
  * routes
-*/ 
-
-exports.index = function(req, res){
-  res.render('index', { title: 'HashOut' });
-};
-<<<<<<< HEAD
+*/
 
 exports.create = function(req, res){
 				res.render('create', {title: 'HashOut'});
 			};
-			
+
 exports.home = function(req, res){
 				res.render('home',{title: 'HashOut'});
 			};
-			
+
 exports.signin = function(req, res){
 				res.render('signin', {title: 'HashOut'})
 			};
-			
+
 exports.signup = function(req, res){
 				res.render('signup', {title: 'HashOut'})
-			};		
-=======
->>>>>>> 99fd36720445d8e7fbc0e0981b93e578785f3be5
+			};
