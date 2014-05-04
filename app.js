55f420d741c5794585d187b6dc823b68b6da9ce8
gtrmjz@gmail.com <gtrmjz@gmail.com>
2014-05-03 22:44:25 -0400

    .

--- app.js
+++ app.js
@@ -7,19 +7,23 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , bodyParser = require('body-parser')
+  , methodOverride = require('method-override');
 
-var app = express();
+var app = express()
+	, router = express.Router();
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+app.use(bodyParser());
+//.use(method-override());
+//app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -27,9 +31,16 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+router.get('/', function(req, res) {
+	res.send('index');
+})
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+//app.get('/', routes.index);
+//app.get('/users', user.list);
+
+
+http.createServer(app).listen(3000);
+
+//(app.get('port'), function(){
+ // console.log('Express server listening on port ' + app.get('port'));
+//});
